dc9d7698ecd79e58c52f99d1f7e1b45139cc1948
daxval <54683150+daxval@users.noreply.github.com>
2021-02-24 11:19:26 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,5 +9,20 @@ function updateCountryView(){
     }
 }
 
-var Dob = document.querySelector("#txtDOB");
-var divDoBError = document.querySelector('#divDoBError');
+function validateForm(){
+
+    var Dob = document.querySelector("#txtDOB");
+    var divDoBError = document.querySelector("#divDoBError");
+
+    if(DoB.value == ""){
+        divDoBError.classList.remove("invisble");
+        divDoBError.innerHTML = "The Date of birth can not be empty."
+        Dob.classList.add("hasError");
+    }else{
+        var DoBDate = new Date (DoB.value);
+        var todayDate = new Date();
+        if(DoBDate >= todayDate){
+            //error
+        }
+    }
+}
